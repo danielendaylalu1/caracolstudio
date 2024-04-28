cccdace2d93ec4e2f139e1135b3f7160931c676f
daniel endaylalu <endaylaludaniel@gmail.com>
2024-04-28 22:05:29 +0300

    added robotics footer

--- src/app/robotics/page.jsx
+++ src/app/robotics/page.jsx
@@ -141,11 +141,11 @@ const page = () => {
           </div>
         </div>
         <div className="bg-[#f0f0f0] flex min-h-[20vh] px-4 items-center justify-center space-x-16 border-2 border-white">
-          <p className="text-sm">
+          <p className="text-xs">
             Via Cavour 2 | Lomazzo (CO) ITALY | CAP 22074
           </p>
-          <p className="text-sm">Join our newsletter</p>
-          <p className="text-sm">0039 | 3311128040 - 0039 | 3491027738</p>
+          <p className="text-xs">Join our newsletter</p>
+          <p className="text-xs">0039 | 3311128040 - 0039 | 3491027738</p>
         </div>
       </div>
     </div>
