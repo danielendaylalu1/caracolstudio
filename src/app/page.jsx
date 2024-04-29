77cda067b3f2f0594314d53f7777110730781972
daniel endaylalu <endaylaludaniel@gmail.com>
2024-04-29 11:09:59 +0300

    fixed header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,29 +7,31 @@ import VideoBg from "@/components/VideoBg";
 export default function Home() {
   return (
     <main className="">
-      <div className="HEADER section-header min-h-[100vh] p-4">
+      <div className="HEADER section-header px-4">
         <VideoBg />
 
-        <div className="HEADERTITLES flex max-[985px]:space-x-0 gap-x-8 gap-y-4 items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
-          <Link
-            href="/"
-            className="header-link max-[685px]:text-[24px] max-[685px]:py-12   text-[56px] py-16 text-center px-8 border border-white font-bold text-white inline-block mt-8"
-          >
-            DESIGN
-          </Link>
-          <div className="flex flex-col space-y-4 items-center">
+        <div className="flex justify-center items-center min-h-[100vh]">
+          <div className="HEADERTITLES flex max-[985px]:space-x-0 gap-x-8 gap-y-4 items-center ">
             <Link
               href="/"
-              className="header-link max-[685px]:text-[24px] max-[685px]:py-12   text-[56px] py-16 text-center px-8 border border-white font-bold text-white inline-block"
+              className="header-link max-[685px]:text-[24px] max-[685px]:py-12   text-[56px] py-16 text-center px-8 border border-white font-bold text-white inline-block mt-8"
             >
-              FACTORY
+              DESIGN
             </Link>
-            <Link
-              href="/robotics"
-              className="header-link max-[685px]:text-[24px] max-[685px]:py-12  text-[56px] py-16 text-center px-8 border border-white font-bold text-white inline-block"
-            >
-              ROBOTIC
-            </Link>
+            <div className="flex flex-col space-y-4 items-center">
+              <Link
+                href="/"
+                className="header-link max-[685px]:text-[24px] max-[685px]:py-12   text-[56px] py-16 text-center px-8 border border-white font-bold text-white inline-block"
+              >
+                FACTORY
+              </Link>
+              <Link
+                href="/robotics"
+                className="header-link max-[685px]:text-[24px] max-[685px]:py-12  text-[56px] py-16 text-center px-8 border border-white font-bold text-white inline-block"
+              >
+                ROBOTIC
+              </Link>
+            </div>
           </div>
         </div>
       </div>
