90d1e66dca525a88c898480cd255e6705762e739
daniel endaylalu <endaylaludaniel@gmail.com>
2024-04-29 10:17:53 +0300

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-[#f0f0f0] flex min-h-[20vh] px-4 items-center justify-center space-x-16 border-2 border-white">
+    <div className="bg-[#f0f0f0] flex min-h-[20vh] px-4 items-center justify-center max-[785px]:space-x-16 space-x-8 border-2 border-white">
       <p className="text-xs">Via Cavour 2 | Lomazzo (CO) ITALY | CAP 22074</p>
       <p className="text-xs">Join our newsletter</p>
       <p className="text-xs">0039 | 3311128040 - 0039 | 3491027738</p>
