a3ad4f030a5fb76cd544436244125a718aee5594
daniel endaylalu <endaylaludaniel@gmail.com>
2024-04-29 12:47:56 +0300

    added about style

--- src/app/robotics/page.jsx
+++ src/app/robotics/page.jsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="overflow-y-scroll no-scrollbar">
-      <div className="bg-[#cdc8c9] min-h-[100vh] flex flex-col justify-center space-y-8 px-4 py-16">
+    <div className="h-[100vh] overflow-y-scroll no-scrollbar">
+      <div className="bg-[#cdc8c9] flex flex-col justify-center space-y-8 px-4 py-16">
         <div className="flex justify-around">
           <div className="flex flex-col space-y-8 py-16">
             <h1 className="text-white text-[54px] font-bold">ROBOTIC</h1>
