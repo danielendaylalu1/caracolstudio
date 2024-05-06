b7ff1df9a1264289464941ca69d6de703871530f
daniel endaylalu <endaylaludaniel@gmail.com>
2024-05-06 09:21:44 +0300

    changed favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,6 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </head>
       <body className={`overflow-x-hidden`}>
         <HomeLogo />
         <NavBar />
